324412068a8d966e6dd525413b209b8a7fad1aae
Una <ubanjanin@gmail.com>
2022-03-03 21:25:32 +0100

    Create the administator table service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { Administrator } from 'entities/administator.entity';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 
 @Controller()
 export class AppController {
+  constructor(
+    private administatorService: AdministratorService
+  ) { }
+  
+  
   @Get()
   getIndex(): string {
     return 'Home page'; //http://localhost:3000/
+  }
+
+  @Get('api/administrator') //http://localhost:3000/api/administrator
+  getAllAdmins(): Promise<Administrator[]>{
+     return this.administatorService.getAll();
   }
  }
