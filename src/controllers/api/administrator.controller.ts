6f7a8f899205248bd3efbeef87ce6c733cf4a086
Una <ubanjanin@gmail.com>
2022-03-10 14:24:04 +0100

    Implement an global API response mechanism

--- src/controllers/api/administrator.controller.ts
+++ src/controllers/api/administrator.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { Administrator } from "entities/administator.entity";
+import { resolve } from "path";
 import { AddAdministratorDto } from "src/dtos/add.administrator.dto";
 import { EditAdministratorDto } from "src/dtos/administrator/edit.administrator.dto";
+import { ApiResponse } from "src/misc/api.response.class";
 import { AdministratorService } from "src/services/administrator/administrator.service";
 
 @Controller('api/administrator')
@@ -18,18 +20,26 @@ export class AdministratorController {
 
       
   @Get(':id') //http://localhost:3000/api/administrator/2/
-  getById(@Param('id')  administatorId: number): Promise<Administrator>{
-     return this.administatorService.getById(administatorId);
+  getById(@Param('id')  administatorId: number): Promise<Administrator | ApiResponse>{
+     return new Promise(async (resolve)=>{
+     let admin = await this.administatorService.getById(administatorId);
+
+     if (admin === undefined){
+        resolve(new ApiResponse("error",-1002));
+     }
+
+     resolve(admin);
+   });
   }
   //PUT http://localhost:3000/api/administrator
   @Put()
-  add(@Body() data: AddAdministratorDto): Promise<Administrator>{
+  add(@Body() data: AddAdministratorDto): Promise<Administrator | ApiResponse>{
      return this.administatorService.add(data);
   }
 
   //POST http://localhost:3000/api/administrator/2/
   @Post(':id')
-  edit(@Param('id') id: number, @Body() data: EditAdministratorDto):Promise<Administrator>{
+  edit(@Param('id') id: number, @Body() data: EditAdministratorDto):Promise<Administrator | ApiResponse>{
       return this.administatorService.editById(id, data);
   }
 }
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administator.entity';
+import { resolve } from 'path';
 import { AppModule } from 'src/app.module';
 import { AddAdministratorDto } from 'src/dtos/add.administrator.dto';
 import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { Repository } from 'typeorm';
 
 
@@ -22,7 +24,7 @@ export class AdministratorService {
       return this.administrator.findOne(id);
     }
  
-    add(data: AddAdministratorDto){
+    add(data: AddAdministratorDto):Promise<Administrator | ApiResponse>{
       const crypto = require('crypto');
 
       const passwordHash = crypto.createHash('sha512');
@@ -34,13 +36,27 @@ export class AdministratorService {
       newAdmin.username = data.username;
       newAdmin.passwordHash = passwordHashString;
 
-      return this.administrator.save(newAdmin);
+      return new Promise((resolve)=> {
+        this.administrator.save(newAdmin)
+        .then(data=> resolve(data))
+        .catch(error => {
+           const response: ApiResponse = new ApiResponse("error",-1001);
+           resolve(response);
+        });
+      });
      
     } 
 
-     async editById(id: number, data:EditAdministratorDto):Promise<Administrator>{
+
+     async editById(id: number, data:EditAdministratorDto):Promise<Administrator | ApiResponse>{
         let admin: Administrator = await this.administrator.findOne(id);
         
+        if (admin === undefined){
+          return new Promise((resolve)=> {
+              resolve(new ApiResponse("error",-1002));
+          });
+        }
+
         const crypto = require('crypto');
         const passwordHash = crypto.createHash('sha512');
         passwordHash.update(data.password);
