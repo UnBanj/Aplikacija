191b239efd64594134dddb16ec5b2a7a54cfe782
Una <ubanjanin@gmail.com>
2022-04-13 16:35:21 +0200

    Implement order management for the administator

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -43,7 +43,11 @@ export class OrderService {
         newOrder.cartId = cartId;
         const savedOrder = await this.order.save(newOrder);
 
-        return await this.order.findOne(savedOrder.orderId,{
+        return await this.getById(savedOrder.orderId);
+    } 
+    
+    async getById(orderId: number){
+        return await this.order.findOne(orderId, {
             relations: [
                 "cart",
                 "cart.user",
@@ -54,5 +58,18 @@ export class OrderService {
 
             ],
         });
-    }   
+    }
+
+    async changeStatus(orderId: number, newStatus:  "rejected" | "accepted" | "shipped" | "pending"){
+        const order = await this.getById(orderId);
+
+        if(!order) {
+            return new ApiResponse("error", -9001, "No such order found.")
+        } 
+        order.status = newStatus;
+
+        await this.order.save(order);
+
+        return await this.getById(orderId);
+    }
 }
